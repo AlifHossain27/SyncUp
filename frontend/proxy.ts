8127b3d9852bfc51c496827b2e686b7fc27353d8
AlifHossain27 <alifh044@gmail.com>
2026-06-20 18:35:28 +0600

    Updating NextJS version

--- frontend/proxy.ts
+++ frontend/proxy.ts
@@ -1,10 +1,17 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const path = request.nextUrl.pathname
 
-  const isPublicPath = path === '/login'
+  const publicPaths = [
+  '/',
+  '/login',
+  '/events',
+  '/newsletter',
+]
+
+  const isPublicPath = publicPaths.includes(path)
   const token = request.cookies.get('access_token')?.value || ''
 
   if (isPublicPath && token) {
@@ -22,6 +29,6 @@ export const config = {
   matcher: [
     '/subscribers',
     '/newsletter/editor',
-    '/profile'
+    '/profile',
   ],
 }
